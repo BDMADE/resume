2dd54593b977d505523b6b337af02f7a274a58d7
hmtanbir <tanbir2025@gmail.com>
2016-08-15 17:37:52 +0600

    model created

--- src/experience/models.py
+++ src/experience/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Experience(models.Model):
+    designation = models.CharField(max_length=250)
+    department = models.CharField(max_length=250, null=True)
+    present = models.BooleanField(default=False)
+    joining_date = models.DateField()
+    ending_date = models.DateField(null=True)
+
+    def __unicode__(self):
+        return self.designation
+
+    def __str__(self):
+        return self.designation
