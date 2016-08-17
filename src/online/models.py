53228fd33465032b1bd3419d27dc6c7a28707117
hta-wtag <hasan.tanbir@welltravel.com>
2016-08-17 18:09:52 +0600

    model created

--- src/online/models.py
+++ src/online/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Online(models.Model):
+    name = models.CharField(max_length=250)
+    link = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
