6bed51b89deeb0daf1758b1f35e4eb5de960e81f
hta-wtag <hasan.tanbir@welltravel.com>
2016-08-17 18:31:35 +0600

    Icon model created

--- src/resume/settings.py
+++ src/resume/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'icon.apps.IconConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
