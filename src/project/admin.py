acfb9673abe1c1f3317c577399b8d5c30a50c04f
hmtanbir <tanbir2025@gmail.com>
2016-08-19 18:50:01 +0600

    models add in admin

--- src/project/admin.py
+++ src/project/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Tool, Gem, ProjectList
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(ProjectList)
+admin.site.register(Tool)
+admin.site.register(Gem)
