881d569fbbe17f2ac088836f14473ffc68edc2be
hmtanbir <tanbir2025@gmail.com>
2016-08-19 21:28:53 +0600

    model updated

--- src/website/models.py
+++ src/website/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
