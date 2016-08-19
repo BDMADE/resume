11817ebbb2c8e0672d14f8b11bffef1cb7e67440
hmtanbir <tanbir2025@gmail.com>
2016-08-19 21:03:13 +0600

    settings updated

--- src/resume/settings.py
+++ src/resume/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contact.apps.ContactConfig',
     'certificate.apps.CertificateConfig',
     'project.apps.ProjectConfig',
     'icon.apps.IconConfig',
