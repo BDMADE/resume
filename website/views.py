d6c1fa77bcda6a6c2c36542870e759ba691c7629
hta-wtag <tanbir2025@gmail.com>
2016-08-28 17:53:20 +0600

    make website

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
+
 # Create your views here.
 def home(request):
-   # return render(request, 'home.html', {})
-     return render(request,'home.html')
+    return render(request, 'website.html', {})
