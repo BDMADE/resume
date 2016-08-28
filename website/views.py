0953181fdcf11d2b5d1d799dcb543a62f75ca075
hta-wtag <tanbir2025@gmail.com>
2016-08-28 18:27:17 +0600

    change website

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from .models import Website
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'website.html', {})
+    name = Website.name
+    context = {'name': name}
+    return render(request, 'website.html', context)
