835e91accedd745b60d952301c713969fedb870d
hta-wtag <tanbir2025@gmail.com>
2016-08-30 14:37:52 +0600

    about page complete

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Website
+from about.models import About
 
 
 # Create your views here.
 def home(request):
     website = Website.objects.last()
-    context = {'website': website}
+    about = About.objects.last()
+    context = {'website': website,
+               'about': about}
     return render(request, 'website.html', context)
