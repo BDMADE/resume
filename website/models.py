bf7ac716a8707030245d472015bf1a22008cf917
hta-wtag <tanbir2025@gmail.com>
2016-08-30 15:34:18 +0600

    title adds in website model

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Website(models.Model):
+    title = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     logo = models.ImageField(max_length=255)
 
