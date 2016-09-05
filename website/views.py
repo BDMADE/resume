cb117c0d2276f4df8bb5dff078149ba700da064b
hta-wtag <tanbir2025@gmail.com>
2016-09-05 16:01:09 +0600

    skill frontend done

--- website/views.py
+++ website/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Website
 from about.models import About
+from skill.models import Skill, Subskill
 
 
 # Create your views here.
 def home(request):
     website = Website.objects.last()
     about = About.objects.last()
+    skills = Skill.objects.all().prefetch_related('subskill_set')
+
     context = {'website': website,
-               'about': about}
+               'about': about,
+               'skills': skills
+               }
     return render(request, 'website.html', context)
