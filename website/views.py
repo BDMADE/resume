1c124c2779f6d784515799b15aacd26f51b5a8db
hmtanbir <tanbir2025@gmail.com>
2016-09-09 21:43:15 +0600

    contact updated

--- website/views.py
+++ website/views.py
@@ -6,6 +6,7 @@ from skill.models import Skill
 from experience.models import Experience
 from project.models import Project
 from certificate.models import Certificate
+from contact.models import Contact
 
 
 # Create your views here.
@@ -16,11 +17,14 @@ def home(request):
     experiences = Experience.objects.all().prefetch_related('todo_set').order_by('created_at').reverse()
     projects = Project.objects.all().prefetch_related('projectlist_set')
     certificates = Certificate.objects.all().order_by('created_at').reverse()
+    contact = Contact.objects.last()
+
     context = {'website': website,
                'about': about,
                'skills': skills,
                'experiences': experiences,
                'projects': projects,
-               'certificates': certificates
+               'certificates': certificates,
+               'contact': contact
                }
     return render(request, 'website.html', context)
