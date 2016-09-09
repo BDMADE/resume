c3eee94f87c7cc959da0670d23486c19f4a3cdcb
hmtanbir <tanbir2025@gmail.com>
2016-09-09 10:51:58 +0600

    timestamp add in about model

--- about/models.py
+++ about/models.py
@@ -8,6 +8,8 @@ class About(models.Model):
     name = models.CharField(max_length=255)
     details = models.TextField(max_length=1500, verbose_name='About')
     cv_link = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __unicode__(self):
         return self.name
