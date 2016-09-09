b4c27f7973775dac4e8d38e70d11b56017251c8a
hmtanbir <tanbir2025@gmail.com>
2016-09-09 10:46:02 +0600

    update certificates view

--- website/views.py
+++ website/views.py
@@ -5,6 +5,7 @@ from about.models import About
 from skill.models import Skill
 from experience.models import Experience
 from project.models import Project
+from certificate.models import Certificate
 
 
 # Create your views here.
@@ -14,10 +15,12 @@ def home(request):
     skills = Skill.objects.all().prefetch_related('subskill_set')
     experiences = Experience.objects.all().prefetch_related('todo_set')
     projects = Project.objects.all().prefetch_related('projectlist_set')
+    certificates = Certificate.objects.all().order_by('name').reverse()
     context = {'website': website,
                'about': about,
                'skills': skills,
                'experiences': experiences,
-               'projects': projects
+               'projects': projects,
+               'certificates': certificates
                }
     return render(request, 'website.html', context)
