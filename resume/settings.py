364bd47420d659fff7962aa7bcac890bedd59e2d
hmtanbir <tanbir2025@gmail.com>
2016-09-12 12:09:27 +0600

    static root add

--- resume/settings.py
+++ resume/settings.py
@@ -144,8 +144,8 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+# STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = [
